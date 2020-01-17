2d5e16d54975b82d301d89cb1d5f2893fa6822c1
priyya0208 <priyya.0208@gmail.com>
2020-01-17 13:36:22 +0530

    1 file modified, 7 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,12 @@ import { ReciepesComponent } from './reciepes/reciepes.component';
 import { RecipeListComponent } from './reciepes/recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './reciepes/recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './reciepes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, HeaderComponent, ReciepesComponent, RecipeListComponent, RecipeDetailComponent, RecipeItemComponent ],
+  declarations: [ AppComponent, HelloComponent, HeaderComponent, ReciepesComponent, RecipeListComponent, RecipeDetailComponent, RecipeItemComponent, ShoppingListComponent, ShoppingEditComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
